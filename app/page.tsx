5e2da0a15abb9b0c92c0885ed2a06e61304378ca
k1105 <kntymgs1105@gmail.com>
2026-01-03 18:50:37 +0900

    revalidateを数値リテラルに変更

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import {getAllEntries} from "@/lib/data";
 import HomeClient from "./HomeClient";
 
-// Enable ISR: revalidate every 24 hours
-export const revalidate = 60 * 60 * 24;
+// Enable ISR: revalidate every 24 hours (86400 seconds)
+export const revalidate = 86400;
 
 export default async function Home() {
   const entries = await getAllEntries();
