cc2740c97fab8dcdba489c13148b6345e8a3f483
k1105 <kntymgs1105@gmail.com>
2026-01-19 00:50:28 +0900

    画像の読み込み

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
       },
+      {
+        protocol: "https",
+        hostname: "prod-files-secure.s3.us-west-2.amazonaws.com",
+      },
     ],
   },
 };
