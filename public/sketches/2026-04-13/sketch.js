436619771a1303c3e4b6c5dbdd535898833d17a3
k1105 <kntymgs1105@gmail.com>
2026-04-14 09:49:46 +0900

    日記とスケッチの更新

--- public/sketches/2026-04-13/sketch.js
+++ public/sketches/2026-04-13/sketch.js
@@ -1,5 +1,8 @@
 let pg;
 let bloomShader;
+let x = 0;
+let y = 0;
+let angle = 0;
 
 function preload() {
   bloomShader = loadShader("bloom.vert", "bloom.frag");
@@ -8,6 +11,8 @@ function preload() {
 function setup() {
   createCanvas(innerWidth, innerHeight, WEBGL);
   pg = createGraphics(innerWidth, innerHeight);
+  x = width / 2;
+  y = height / 2;
   noStroke();
 }
 
@@ -15,7 +20,10 @@ function draw() {
   pg.background(0);
   pg.noStroke();
   pg.fill(255);
-  pg.circle(mouseX, mouseY, 100);
+  angle = noise(frameCount / 1000) - 0.5;
+  x += cos(angle);
+  y += sin(angle);
+  pg.circle(x, y, y * sin(y / 1000));
   pg.fill(0);
   for (let i = 0; i < 10; i++) {
     for (let j = 0; j < 10; j++) {
