70800468262ec5fbafd13de69f6e53284746c472
k1105 <kntymgs1105@gmail.com>
2026-07-29 00:27:54 +0900

    追加

--- public/sketches/2026-07-28/sketch.js
+++ public/sketches/2026-07-28/sketch.js
@@ -7,23 +7,20 @@ function setup() {
 }
 
 function draw() {
+  // background(255,1);
   noStroke();
-  fill(0, 0, 0);
+  fill(255*sin(PI*frameCount/100));
   const init_pos = {x: width/2, y: height/2};
-  const randVal = {x: width/2 * noise(frameCount/100), y: 4 * noise(frameCount/100)};
-
+  const randVal = {x: width/2 * noise(frameCount/100), y: noise(frameCount/500)/100};
   push();
   translate(init_pos.x, init_pos.y);
-  rotate(frameCount/1000);
+  rotate(frameCount/100);
   for(let i=0; i<1000; i++) {
-    const pos = {x: i * randVal.x, y: i**2 * randVal.y};
+    const pos = {x: i, y: i**2 * randVal.y};
     if(abs(pos.x) > 0) {
     circle(pos.x, -pos.y, r*k**i);
-    rotate(0.1);
     circle(-pos.x, -pos.y, r*k**i);
-    rotate(0.1);
     circle(pos.x, pos.y, r*k**i);
-    rotate(0.1);
     circle(-pos.x, pos.y, r*k**i);      
     }
   }
